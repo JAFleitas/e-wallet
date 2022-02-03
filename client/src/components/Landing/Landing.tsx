06564da11b3944d36abe87d0a3d6096c5bd6576b
Francisco Alderete <franalderete7@gmail.com>
2022-02-03 14:47:29 -0300

    second commit

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -2,12 +2,18 @@ import React from "react";
 // Add components below
 import Hero from "../HeroSection/Hero";
 import Navigation from "../Navigation/Navigation";
+import NewsLetter from "../Newsletter/Newsletter";
+import Statistics from "../Statistics/Statistics";
+import Footer from "../Footer/Footer";
 
 const Landing: React.FC = () => {
   return (
     <React.Fragment>
       <Navigation />
       <Hero />
+      <Statistics />
+      <NewsLetter />
+      <Footer />
     </React.Fragment>
   );
 };
