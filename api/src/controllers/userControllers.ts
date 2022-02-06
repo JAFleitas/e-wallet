2dc2153f05de634dfdfd5f9709b977671ccc5cec
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-02-06 20:31:14 -0300

    se creo una ruta para crear usuarios en la base de dato

--- api/src/controllers/userControllers.ts
+++ api/src/controllers/userControllers.ts
@@ -1,12 +1,33 @@
-import { Response, Request } from "express";
+import bcrypt from "bcrypt";
+import { Response } from "express";
+import { ReqCreateUser } from "../interface/attributesUser";
 
 const { user } = require("../../db");
 
-const createUser = async (req: Request, res: Response) => {
-  const { name, username, email, password } = req.body;
+const createUser = async (
+  req: ReqCreateUser,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { name, username, email, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
 
-  const newUser = await user.create({ name, username, email, password });
+    const [newUser, created] = await user.findOrCreate({
+      where: { email },
+      defaults: {
+        name,
+        username,
+        password: passwordHash,
+      },
+    });
+    if (created) {
+      return res.json(newUser);
+    }
 
-  res.json(newUser);
+    return res.send("There is already a user with that email!");
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500).send("Internal Error Server");
+  }
 };
 export default createUser;
